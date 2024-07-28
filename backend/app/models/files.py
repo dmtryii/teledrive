a72c91a46b731c275f319bec19ceea095c94cda2
Dmytro Trokhymenko <dmitriy04tr@gmail.com>
2024-07-28 22:51:27 +0300

    Added file upload time

--- backend/app/models/files.py
+++ backend/app/models/files.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.dialects.postgresql import JSON
@@ -9,6 +10,7 @@ class File(db.Model, SerializerMixin):
     __tablename__ = 'file'
 
     id = db.Column(db.BigInteger, primary_key=True)
+    upload = db.Column(db.DateTime, default=datetime.now, nullable=False)
     user_id = db.Column(db.BigInteger, db.ForeignKey('base_user.id'), nullable=False)
     document_info = db.Column(JSON, nullable=False)
 
