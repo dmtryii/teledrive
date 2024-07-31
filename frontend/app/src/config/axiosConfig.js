90190cb420c76373305f318e5d4a5dd8bd6dae09
Dmytro Trokhymenko <dmitriy04tr@gmail.com>
2024-07-31 15:19:24 +0300

    Added docker and docker-compose

--- frontend/app/src/config/axiosConfig.js
+++ frontend/app/src/config/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:5001/',
+  baseURL: 'http://backend:5001/',
 });
 
 axiosInstance.interceptors.request.use(
